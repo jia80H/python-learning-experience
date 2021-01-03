267f55e36a025c77fca92c31a7d27c3bcdb257ad
jia80H <mlfyiwmdf@gmail.com>
2021-01-03 11:32:53 +0800

    Create 02标识符和关键字.py
    规则与规范

---
+++ 第一章 基础概念/02标识符和关键字.py
@@ -0,0 +1,22 @@
+'''
+标识符：变量，模块名，函数名，类名
+标识符的命名规则与规范：
+    规则：
+    1.由 数字 字母 下划线(_) 不开头的数字 组成
+    2.严格区分大小写 a和A
+    3.不能使用关键字 True if
+    规范
+    1.顾名思义 定义变量能让人看懂变量代表什么 a = 12 和num1 = 12
+    2.命名规范
+        小驼峰命名法：第一个单词首字母小写，以后每个单词首字母都大写如userNameAndersonPassword
+        大驼峰命名法：每个单词首字母都大写 PersonModel
+        使用下划线连接：user_name_and_password
+        在python中变量、函数和模块用下划线连接；类名用大驼峰法
+'''
+'''
+常用关键字
+False None True and as assert break class
+continue def elif else except finally for
+from global if import in is lambda nonlocal
+not or pass raise return try while with yield
+'''
