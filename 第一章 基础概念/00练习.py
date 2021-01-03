4c2f868956a293e955af2ad7edd629aa16b1088b
jia80H <mlfyiwmdf@gmail.com>
2021-01-03 18:34:52 +0800

    Update 00练习.py

--- 第一章 基础概念/00练习.py
+++ 第一章 基础概念/00练习.py
@@ -1,5 +1,6 @@
-a = '123cd'
-b = int(a,16)
-print(int(a,16)) #识别含字母的数字 int() base must be >= 2 and <= 36, or 0
-print(bin(b))
-
+if 0:
+    print('false')
+    else:
+        pass
+        print('true')
+        
