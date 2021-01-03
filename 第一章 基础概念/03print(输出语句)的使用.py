bdd907f88b6762002a0ee0fdc147723c0f1cf568
jia80H <mlfyiwmdf@gmail.com>
2021-01-03 16:13:55 +0800

    dd

---
+++ 第一章 基础概念/03print(输出语句)的使用.py
@@ -0,0 +1,45 @@
+#print输出语句
+print('hello world!')
+
+''' print的使用
+def print(
+         *values: object, 
+         sep: Optional[Text] = ..., #默认空格为分隔符
+         end: Optional[Text] = ...,
+             #输出完print语句后，默认输出的字符为'/n'（换行）
+         file: Optional[_Writer] = ...,
+             #改变存的位置
+         flush: bool = ...
+             
+    )
+ -> None: ...
+'''
+
+print('hello','good','yes')
+
+'''
+>>> print('hello','good','yes')
+hello good yes
+>>>
+'''
+
+
+print('hello','good','yes','hi',sep = '+')
+
+'''
+>>> print('hello','good','yes','hi',sep = '+')
+hello+good+yes+hi
+>>>
+'''
+print('hello','good','yes')
+print('hello','good','yes')
+'''
+hello good yes
+hello good yes
+'''
+print('hello','good','yes','hi',sep = '+',end = "\t")
+print('hello','good','yes','hi',sep = '+',end = "&&*&*%^&$%^")
+
+'''
+hello+good+yes+hi       hello+good+yes+hi&&*&*%^&$%^
+'''
