e8d7a2a786b83d466845d172969349323b8a855d
jia80H <mlfyiwmdf@gmail.com>
2021-01-04 12:02:16 +0800

    Create 07运算符.py

---
+++ 第一章 基础概念/07运算符.py
@@ -0,0 +1,32 @@
+import os
+#*算数运算符*
+
+
+#数字运算
+# + - * /  **幂运算 //整除 %取余
+
+print(1+1) #2
+
+# py2里，两整数相除，得到的是一个int 9/2=4
+# py3里，两整数相除，得到的是一个float
+print(6 / 2) #3.0
+
+print(81**(1/2)) #9.0
+
+print(10//3) #3 取整
+
+print(10%3) #1 取余
+
+#字符串运算
+#有限度的支持加法和乘法
+#加法运算符：拼接两个字符串
+print('hello'+'world') #helloworld
+
+#乘法运算符：重复字符串
+print('hello' * 2) #hellohello
+
+
+
+os.system('pause')
+
+
