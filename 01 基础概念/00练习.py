8e23dbeb987a2d3a34326cbbedf23d0d46fa163c
jia80H <mlfyiwmdf@gmail.com>
2021-01-05 18:49:47 +0800

    Update 00练习.py

--- 01 基础概念/00练习.py
+++ 01 基础概念/00练习.py
@@ -1,8 +1,5 @@
- numColor = 0xF0384E
- exB = numColor >> 8
- red = exB >> 8
- red00 = red << 16
- exR = red00 ^ numColor
- green = exR >> 8
- blue = exR ^ green
- print('红色',red,'绿色',green.'蓝色'.blue)
+color = oxF0384E
+red = color >> 16
+green = color >> 8 & 0xFF
+blue = color & 0xFF
+print(red,green,blue)
