fb8f4acaaccd78252118ce7f0cf7719e28db3a12
jia80H <mlfyiwmdf@gmail.com>
2021-01-06 17:54:00 +0800

    Create 01条件判断语句.py

---
+++ 02 语句/01条件判断语句.py
@@ -0,0 +1,20 @@
+'''if'''
+#if 和if else 以及if elif elif else
+
+#if 条件判断 ：
+#   条件成立时，执行的代码
+age = int(input('请输入你的年龄：'))
+if age < 18:
+    print('未满十八禁止入内')
+
+#if...else
+#if 条件判断 ：
+#   条件成立时，执行的代码
+#else:
+#   条件不成立时，执行的代码
+age = int(input('请输入你的年龄：'))
+if age < 18:
+    print('未满十八禁止入内')
+else:
+    print('欢迎光临')
+
