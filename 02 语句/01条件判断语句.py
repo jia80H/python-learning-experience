83620a666040769e072b2254ca10f3a9ba565464
jia80H <mlfyiwmdf@gmail.com>
2021-01-07 07:35:07 +0800

    Update 01条件判断语句.py

--- 02 语句/01条件判断语句.py
+++ 02 语句/01条件判断语句.py
@@ -18,3 +18,15 @@ if age < 18:
 else:
     print('欢迎光临')
 
+
+#if elif elif else
+score = float(input('请输入你的成绩：'))
+if 0<=score<60:
+    print('不及格，重修')
+    elif 60<=score<80:
+        print('及格了，但是一般般')
+        elif 80<=80<90:
+            print('贡献良好，但是你还能更优秀')
+            else：
+                print('优秀！满绩点！恭喜')
+            
