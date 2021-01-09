d7976f6936e023be45f43fc77aee5fb15bd91987
jia80H <mlfyiwmdf@gmail.com>
2021-01-09 10:45:35 +0800

    Update 00练习.py

--- 00练习.py
+++ 00练习.py
@@ -1,11 +1,5 @@
-#if elif elif else
-score = float(input('请输入你的成绩：'))
-if 0<=score<60:
-    print('不及格，重修')
-elif 60<=score<80:
-    print('及格了，但是一般般')
-elif 80<=score<90:
-    print('贡献良好，但是你还能更优秀')
-else :
-    print('优秀！满绩点！恭喜')
-         
+'''三元表达式'''
+num1=int(input('数字1'))
+num2=int(input('数字2'))
+x = num1 if num1>num2 else num2
+print(x)
