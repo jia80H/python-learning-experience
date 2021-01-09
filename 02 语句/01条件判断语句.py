9aea6fecf96b46a87e22fa3ead17ab411b179b88
jia80H <mlfyiwmdf@gmail.com>
2021-01-09 10:45:47 +0800

    Update 01条件判断语句.py

--- 02 语句/01条件判断语句.py
+++ 02 语句/01条件判断语句.py
@@ -21,7 +21,7 @@ else:
 
 #if elif elif else
 score = float(input('请输入你的成绩：'))
-if 0<=score<60:
+if 0<=score<60: #有的语言不能连续比较大小，必须用逻辑连接符 0<=score and score<60
     print('不及格，重修')
 elif 60<=score<80:
     print('及格了，但是一般般')
@@ -29,3 +29,23 @@ elif 80<=score<90:
     print('贡献良好，但是你还能更优秀')
 else :
     print('优秀！满绩点！恭喜')
+
+'''if的镶嵌'''
+tick = input('买票了吗？Y/N\n')
+if tick == 'Y':
+    safe = input('安检通过了吗？Y/N\n')
+    if safe=='Y':
+        print('欢迎进站\n')
+    else:
+        print('再次安检\n')
+else:
+    print('买票去\n')
+    pass
+
+#pass没有意义，用来占位
+
+'''三元表达式'''
+num1=int(input('数字1'))
+num2=int(input('数字2'))
+x = num1 if num1>num2 else num2
+print(x)
