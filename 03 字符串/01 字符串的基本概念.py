3dc02aadac5c0e5cbd4fe63fbb2421b35c955101
jia80H <mlfy_jia@outlook.com>
2021-01-15 10:34:38 +0800

    Create 01 字符串的基本概念.py

---
+++ 03 字符串/01 字符串的基本概念.py
@@ -0,0 +1,16 @@
+'''以下都是字符串'''
+a = 'hello'
+b = "goog"
+c = '''呵呵'''
+d = """hhhh"""
+
+#如果字符串中有双引号，外边就可以使用单引号（同理其他类似）
+print('xiaoming said:"i love you"')
+
+'''转义字符 \ '''
+x = 'i\'m xiao ming'
+#\' 显示单引号
+
+#在字符串前面加r 在python里表示原生字符
+print(r'\dafdsf\ndsaffs')
+
