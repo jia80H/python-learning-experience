4bdd26f3e48ddbcd82f8635bd3abf4a188e99615
jia80H <mlfy_jia@outlook.com>
2021-01-16 08:22:34 +0800

    Create 01 列表的基本使用.py

---
+++ 04 列表list/01 列表的基本使用.py
@@ -0,0 +1,16 @@
+#使用[]来表示一个列表，列表里的每一个数据称为元素
+#元素之间使用都好进行分隔
+
+names = ['张三','李四','王五','joy','linda','sazi']
+
+names = list(('张三','李四','王五','joy','linda','sazi'))
+print(names)
+
+# 列表和字符串一样，都可以用下标来获取元素和对象
+#同时，我们可以使用下标来修改列表里的元素
+
+names = list(('张三','李四','王五','joy','linda','sazi'))
+print(names[3]) #joy
+names[3] = '花木兰'
+print(names) # ['张三', '李四', '王五', '花木兰', 'linda', 'sazi']
+print(names[3:5])
