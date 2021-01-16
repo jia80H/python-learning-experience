a07df9e899f84e3af5482ad6d068492c8bd30cec
jia80H <mlfy_jia@outlook.com>
2021-01-16 09:49:47 +0800

    Update 00练习.py

--- 00练习.py
+++ 00练习.py
@@ -1,6 +1,4 @@
-word = 'hello'
-m = input('zifu')
-if m in word :
-    print('cunzai')
-else:
-    print('bucunzai')
+#sorted(nums) 内置函数sorted
+numbs = [6,5,3,1,8,7,2,4]
+a = sorted(numbs)
+print(a)
