864870eb7811c8d9f7a6e9f7e209c63f7eec0028
jia80H <mlfy_jia@outlook.com>
2021-01-18 07:45:03 +0800

    Update 01 元组.py

--- 04 元组、字典、集合/01 元组.py
+++ 04 元组、字典、集合/01 元组.py
@@ -33,3 +33,9 @@ print('*'.join(heights))
 nums = (1,9,3,2,5,8,9)
 for i in nums:
     print(i)
+
+#元组可以使用+来合并
+nums = (1,9,3,2,5,8,9)
+nums1 = (10,93,31,22,51,81,91)
+nums2 = nums+nums1
+print(nums2)
