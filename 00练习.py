f7b6edb141e57d6ed16db4b4e6dc126518d9a2a1
jia80H <mlfy_jia@outlook.com>
2021-01-19 10:10:22 +0800

    Update 00练习.py

--- 00练习.py
+++ 00练习.py
@@ -1,16 +1,18 @@
-# 更优
-chars = ['a','b','c','w','s','a','w','d','w','a','x','v','x']
-char_count = {}
-for char in chars:
-    if char not in char_count:
-        char_count[char] = chars.count(char)
-print(char_count)
+students=[
+    {'name':'张三','age':18,'score':98,'tel':'17335463210','gender':'female'},
+    {'name':'李四','age':28,'score':95,'tel':'12734632108','gender':'male'},
+    {'name':'王五','age':21,'score':98,'tel':'16835463210','gender':'unknown'},
+    {'name':'chris','age':17,'score':58,'tel':'17835486108','gender':'male'},
+    {'name':'jack','age':23,'score':52,'tel':'1883546808','gender':'female'},
+    {'name':'tony','age':15,'score':89,'tel':'17835463210','gender':'unknown'}
+]
 
-# 找到最多的
-vs = char_count.values()
-# 可以使用内置函数max取最大数
-max_count = max(vs)
 
-for k,v in char_count.items():
-    if v == max_count:
-        print(k)
+
+i = 0
+while i < len(students):
+    if students[i]['gender'] == 'unknown':
+        students.remove(students[i])
+        i -= 1
+    i += 1
+print(students)
