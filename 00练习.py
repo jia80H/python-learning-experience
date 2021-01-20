81a6adee060c3a4e19222fd3651413c3509686cf
jia80H <mlfy_jia@outlook.com>
2021-01-20 11:31:06 +0800

    Update 00练习.py

--- 00练习.py
+++ 00练习.py
@@ -1,18 +1,9 @@
-students=[
-    {'name':'张三','age':18,'score':98,'tel':'17335463210','gender':'female'},
-    {'name':'李四','age':28,'score':95,'tel':'12734632108','gender':'male'},
-    {'name':'王五','age':21,'score':98,'tel':'16835463210','gender':'unknown'},
-    {'name':'chris','age':17,'score':58,'tel':'17835486108','gender':'male'},
-    {'name':'jack','age':23,'score':52,'tel':'1883546808','gender':'female'},
-    {'name':'tony','age':15,'score':89,'tel':'17835463210','gender':'unknown'}
-]
-
-
-
-i = 0
-while i < len(students):
-    if students[i]['gender'] == 'unknown':
-        students.remove(students[i])
-        i -= 1
-    i += 1
-print(students)
+sing = ('a','b','c','d','e','f','g')
+dance = ('g','f','s','d','a','r','c')
+rap = ('g','e','y','i','t','h','r','a')
+p_dict = {}
+allPerson = sing + dance + rap
+for name in allPerson:
+    if name not in p_dict:
+        p_dict[name] = allPerson.count(name)
+print(p_dict)
