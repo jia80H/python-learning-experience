b5aa9d09cc387f9154f63cf2ac640565e1af98ee
jia80H <mlfy_jia@outlook.com>
2021-01-24 11:43:07 +0800

    Revert "2021/1/24"
    This reverts commit 6cf73da2e84caed0aec931d67ecd1ee327ece932.

--- 00练习.py
+++ 00练习.py
@@ -1,20 +1,6 @@
-class Student(object):
-    def __init__(self,x,y):
-        self.name = x
-        self.age = y
+_x = 100
 
-    def say_hello(self):
-        print('大家好，我是',self.name)
+# 最后删除
+del _x
 
-
-# Student('张三'，18) 这段代码做了什么？
-# 1.调用__new__方法，用来申请内存空间
-# 2.调用__init__方法传入参数，将self指向创建好的内存空间，填充数据
-# 3.变量s1 也指向创建好的内存空间
-s1 = Student('张三', 18)
-s1 = Student('张三', 18)
-s1.say_hello()
-
-# 申请一块新的内存空间
-s2 = Student('jack',18)
-s2.say_hello()
+print(_x)
