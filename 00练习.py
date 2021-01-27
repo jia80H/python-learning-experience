2442152914558d9dbfaae4a2f810009f73477b1c
jia80H <mlfy_jia@outlook.com>
2021-01-27 10:05:16 +0800

    2021、1、27

--- 00练习.py
+++ 00练习.py
@@ -1,20 +1,38 @@
-class Singleton(object):
-    __instance = None 
-    __is_first = True
+class Dog(object):
+    def work(self):
+        print('狗正在工作')
+class pDog(Dog):
+    def attack_enemy(self):
+        print('警犬正在攻击坏人')
 
-    @classmethod
-    def __new__(cls, *rgs, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = object.__new__(cls)
-        return cls.__instance
 
-    def __init__(self,a,b):
-        if self.__is_first:
-            self.a = a
-            self.b = b
-            self.__is_first = False
+class Bdog(Dog):
+    def zhilu(self):
+        print('导盲犬正在指路')
 
-s1 = Singleton('aaaa', 'bbbbb')
-s2 = Singleton('hhhh', 'lllll') 
 
-print(s2.a) # aaaa
+class Ddog(Dog):
+    def jidu(self):
+        print('缉毒犬正在搜毒')
+
+
+class Person(object):
+    def __init__(self,name):
+        self.name = name
+
+    def work_with_dog(self):
+        self.dog.work()
+
+p = Person('张三')
+
+pd = pDog()
+p.dog = pd
+p.work_with_dog()
+
+bd = Bdog()
+p.dog = bd
+p.work_with_dog()
+
+dd = Ddog()
+p.dog = dd
+p.work_with_dog()
---
+++ 08 面向对象/12 多态的使用.py
@@ -0,0 +1,92 @@
+# 多态基于继承, 通过子类重写父类的方法
+# 达到不同子类调用相同父类方法,得到不同结果
+# 提高代码的灵活度
+""" 不使用多态 """
+class pDog(object):
+    def attack_enemy(self):
+        print('警犬正在攻击坏人')
+
+
+class Bdog(object):
+    def zhilu(self):
+        print('导盲犬正在指路')
+
+
+class Ddog(object):
+    def jidu(self):
+        print('缉毒犬正在搜毒')
+
+
+class Person(object):
+    def __init__(self,name):
+        self.name = name
+
+    def work_with_pd(self):
+        self.dog.attack_enemy()
+
+    def work_with_bd(self):
+        self.dog.zhilu()
+
+    def work_with_dd(self):
+        self.dog.jidu()
+
+
+p = Person('张警官')
+
+pd = pDog()
+p.dog = pd
+p.work_with_pd()
+
+bd = Bdog()
+p.dog = bd
+p.work_with_bd()
+
+dd = Ddog()
+p.dog = dd
+p.work_with_dd()
+
+""" 使用多态 """
+
+
+class Dog(object):
+    def work(self):
+        print('狗正在工作')
+
+
+class pDog(Dog):
+    def work(self):
+        print('警犬正在攻击坏人')
+
+
+class Bdog(Dog):
+    def work(self):
+        print('导盲犬正在指路')
+
+
+class Ddog(Dog):
+    def work(self):
+        print('缉毒犬正在搜毒')
+
+
+class Person(object):
+    def __init__(self,name):
+        self.name = name
+        self.dog = None
+
+    def work_with_dog(self):
+        if isinstance(self.dog,Dog):
+            self.dog.work()
+
+p = Person('张三')
+
+pd = pDog()
+# p.dog = pd
+p.work_with_dog()
+
+bd = Bdog()
+p.dog = bd
+p.work_with_dog()
+
+dd = Ddog()
+p.dog = dd
+p.work_with_dog()
