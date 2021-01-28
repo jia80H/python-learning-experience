f74e6a7cacbb24e9ea327a93233e8f5915f00a8c
jia80H <mlfy_jia@outlook.com>
2021-01-28 09:33:35 +0800

    Update 00练习.py

--- 00练习.py
+++ 00练习.py
@@ -1,38 +1,11 @@
-class Dog(object):
-    def work(self):
-        print('狗正在工作')
-class pDog(Dog):
-    def attack_enemy(self):
-        print('警犬正在攻击坏人')
-
-
-class Bdog(Dog):
-    def zhilu(self):
-        print('导盲犬正在指路')
-
-
-class Ddog(Dog):
-    def jidu(self):
-        print('缉毒犬正在搜毒')
-
-
-class Person(object):
-    def __init__(self,name):
-        self.name = name
-
-    def work_with_dog(self):
-        self.dog.work()
-
-p = Person('张三')
-
-pd = pDog()
-p.dog = pd
-p.work_with_dog()
-
-bd = Bdog()
-p.dog = bd
-p.work_with_dog()
-
-dd = Ddog()
-p.dog = dd
-p.work_with_dog()
+money = input('输入指定格式转换')
+if 'RMB' in money:
+    num = float(money[3:])
+    usd = round(num / 6.78,2)
+    print('{}与USD{}等值'.format(money,usd))
+elif 'USD' in money:
+    num = float(money[3:])
+    rmb = round(num * 6.78,2)
+    print('{}与RMB{}等值'.format(money,rmb))
+else:
+    print('错误格式输入')
